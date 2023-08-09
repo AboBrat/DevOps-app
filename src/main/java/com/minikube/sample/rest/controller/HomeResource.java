2f3cc6c7c0d23e836704f32f145ad8ae71dad77c
AboBrat <nikola.drobnjak999@gmail.com>
2023-08-09 02:18:43 +0200

    update

--- src/main/java/com/minikube/sample/rest/controller/HomeResource.java
+++ src/main/java/com/minikube/sample/rest/controller/HomeResource.java
@@ -1,17 +1,16 @@
 package com.minikube.sample.rest.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.minikube.sample.properties.PropertiesConfig;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Lookup;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.minikube.sample.properties.PropertiesConfig;
+
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * @author Gorantla, Eresh
